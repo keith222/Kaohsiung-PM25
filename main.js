b45991a9acf559fda83ba0d1270865878765a8d7
Yang Tun-Kai <ks97121@gmail.com>
2015-12-08 01:31:26 +0800

    update js file
    add array to collect data

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+var site = [];
+var polltion = []
+
 $( document ).ready(function() {
-	$.getJSON('http://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000001?sort=SiteName&offset=0&limit=12&filters={%22County%22:%22%E9%AB%98%E9%9B%84%E5%B8%82%22}&callback=?',function(result){
-		console.log(result);
-	})
+	
+	getJsonData();
+
 });
 
+
+function getJsonData(){
+	var url = 'http://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000001?sort=SiteName&offset=0&limit=12&filters={%22County%22:%22%E9%AB%98%E9%9B%84%E5%B8%82%22}&callback=?'
+	$.getJSON(url,function(result){
+		var record = result.result.records
+
+		for(var k in record){
+			site[k] = record[k].SiteName;
+			pollution[k] = record[k]["PM2.5"]
+		}
+	})
+
+
+};
 
